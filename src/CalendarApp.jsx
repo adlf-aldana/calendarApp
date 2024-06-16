a872db2bb037db14c0ba0a109c2f999d8cb36187
Adolfo Aldana <adlf.aldana@gmail.com>
2024-06-15 23:16:28 -0400

    Add. Application paths

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,5 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRouter } from "./router/";
+
 const CalendarApp = () => {
-  return <div>CalendarApp</div>;
+  return (
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
+  );
 };
 
 export default CalendarApp;
