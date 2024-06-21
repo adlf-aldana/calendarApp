91902b92a5eeb357bab5652716207af3621d29c4
Adolfo Aldana <adlf.aldana@gmail.com>
2024-06-21 12:04:54 -0400

    #Add. Edit the active event

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { onAddNewEvent, onSetActiveEvent } from "../store/";
+import { onAddNewEvent, onSetActiveEvent, onUpdateEvent } from "../store/";
 
 export const useCalendarStore = () => {
   const dispatch = useDispatch();
@@ -10,6 +10,7 @@ export const useCalendarStore = () => {
   const startSavingEvent = async (calendarEvent) => {
     if (calendarEvent._id) {
       //Actualizando
+      dispatch(onUpdateEvent({ ...calendarEvent }));
     } else {
       //Creando
       dispatch(onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }));
