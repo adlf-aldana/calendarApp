a9bf038627292ed7bafaa043c97d9c5993aa2dc7
Adolfo Aldana <adlf.aldana@gmail.com>
2024-07-04 23:20:52 -0400

    #Add. Creating environment variables

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,11 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth/";
 import { CalendarPage } from "../calendar/";
+import { getEnvVariables } from "../helpers";
 
 export const AppRouter = () => {
   const authStatus = "authenticated"; //"authenticated" "not-authenticated"
+  console.log(getEnvVariables());
   return (
     <Routes>
       {authStatus === "not-authenticated" ? (
